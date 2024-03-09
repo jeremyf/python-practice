a41f480e897f98d088dfc49aaa772ff3abb9f026
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-03-09 13:14:06 -0500

    Updating README

---
+++ iterator.py
@@ -0,0 +1,14 @@
+# some_primes = [2, 3, 5]
+# for prime in some_primes:
+#     print(prime)
+
+# Dictionary in Python
+d = {'H': 1.008, 'He': 4.003, 'Li': 6.94}
+print(d)
+for symbol, weight in d.items():
+    print(symbol, weight)
+
+# This is analogous to Ruby's #each_with_object({})
+print({symbol: weight for symbol, weight in d.items() if weight > 5})
+
+print(list(symbol for symbol, weight in d.items() if weight > 5))
